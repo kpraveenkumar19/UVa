51bbcb1b5e26fc5a7474ee1ed8f0f7e53b57278f
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2023-09-29 01:19:01 +0530

    Update 10978 - Let's Play Magic.cpp

--- volume109/10978 - Let's Play Magic.cpp
+++ volume109/10978 - Let's Play Magic.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main() {
     int n;
     while(scanf("%d", &n) == 1 && n) {
-        string ret[n];
-        string s, foo;
-        int i, idx = 0, len;
+        string ret[n];  // declaration of array of string to store the resultant array of cards
+        string s, foo;  // string s stores the name of card which will be again stored in ret array 
+        int i, idx = 0, len;// idx deontes next vacant space or (idx-1)position denotes position in which card s should be placed
         for(i = 0; i < n; i++) {
             cin >> s >> foo;
             len = foo.length();
-            while(len) {
-                if(ret[idx] == "")
+            while(len) {//it moves len vacant position from current vacant position that is idx and it reaches next vacant position
+                if(ret[idx] == "")// if it is unoccupied only then it should counted
                     len--;
                 idx++;
-                if(idx >= n)    idx = 0;
+                if(idx >= n)    idx = 0;//cyclic nature
             }
-            ret[(idx-1+n)%n] = s;
+            ret[(idx-1+n)%n] = s;// edge case if idx is 0 then idx -1 = -1 but it should be n-1
         }
         for(i = 0; i < n; i++) {
             if(i)   putchar(' ');
