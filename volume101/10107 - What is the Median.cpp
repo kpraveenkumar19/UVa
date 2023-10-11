b45ca849f34a92d534235b9bba4744dcf01e9472
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2023-10-11 10:52:07 +0530

    Update 10107 - What is the Median.cpp

--- volume101/10107 - What is the Median.cpp
+++ volume101/10107 - What is the Median.cpp
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main() {
-	int n = -1, A[10001], tmp, i;
+	int n = 0, A[10001], tmp, i;
 	while(scanf("%d", &tmp) == 1) {
-		for(i = n; i >= 0; i--)
+		for(i = n-1; i >= 0; i--)
 			if(A[i] > tmp)
 				A[i+1] = A[i];
 			else	break;
 		A[i+1] = tmp;
 		n++;
 		if(n&1)
-			printf("%d\n", (A[n/2] + A[n/2+1])/2);
+			printf("%d\n", (A[n/2-1] + A[n/2])/2);
 		else
-			printf("%d\n", A[n/2]);
+			printf("%d\n", A[n/2-1]);
 	}
     return 0;
 }
